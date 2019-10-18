6a95623a9bea4b2d36afd9b19ba8bb72eef08e2b
Matt LeRoy <mleroy@minitab.com>
2019-10-18 17:02:18 -0400

    add `usage` function, check arg is a directory

--- archive_recording.sh
+++ archive_recording.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+usage () {
+    echo "usage: ${0##*/} path_to_recording_folder"
+}
+
 if [[ $# -ne 1 ]]; then
     echo "error: Invalid number of arguments."
-    echo "usage: ${0##*/} path_to_recording_folder"
+    usage
     exit -1
 fi
+
+if [ ! -d "$1" ]; then
+    echo "error: \"$1\" not a valid directory path."
+    usage
+    exit -2
+fi
+DIR="$1"
