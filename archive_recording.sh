ef4c225bc4f851d82eed0798b422b66ccfe4822a
Matt LeRoy <mleroy@minitab.com>
2019-10-21 09:04:24 -0400

    fully functional

--- archive_recording.sh
+++ archive_recording.sh
@@ -15,8 +15,27 @@ if [ ! -d "$1" ]; then
     usage
     exit -2
 fi
-DIR="$1"
 
 command -v flac >/dev/null 2>&1 || { echo >&2 "flac encoder doesn't appear to be installed.  Aborting."; exit 1; }
 
-find $DIR -type f -iname "*.wav" -exec flac -4V --keep-foreign-metadata {} \;
+cd "$1"
+DIR=$(basename "$(pwd)")
+cd ..
+
+echo "Compressing *.wav files in $(pwd)/$DIR ...\n"
+
+NUM_WAVS=$(find "$DIR" -type f -iname "*.wav" | wc -l)
+if [ $NUM_WAVS -eq 0 ]; then
+    echo "warning: no *.wav files found."
+else
+    find "$DIR" -type f -iname "*.wav" -exec flac -4V --keep-foreign-metadata --delete-input-file {} \;
+fi
+
+ARCHIVE_NAME="$DIR.7z"
+echo "Archiving directory to file $ARCHIVE_NAME ...\n"
+
+if [ -f "$ARCHIVE_NAME" ]; then
+    echo "error: 7z archive $ARCHIVE_NAME already exists; aborting."
+else
+    7zr a "$ARCHIVE_NAME" "$DIR"
+fi
